4a62230425a3e48dad45de0ad881b69ab9a1baba
cav <cav@cav>
2019-09-11 15:07:00 -0400

    more structure

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sat-examples
 Simple examples of using SAT solvers
+
+# build
+build the tools using: ./build.sh
---
+++ build.sh
@@ -0,0 +1,4 @@
+set -x
+gcc tools/drat-trim.c    -O2 -o tools/drat-trim
+gcc tools/color-encode.c -O2 -o tools/color-encode
+
