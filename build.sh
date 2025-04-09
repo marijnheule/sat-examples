580b1b98c3acc4cd58a3cbfad01546fbf1ff41c5
Marijn Heule <mheule@Marijns-MacBook-Air-2.local>
2025-04-08 20:28:14 -0700

    ptn

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 set -x
+gcc encode/PythagoreanTriples.c    -O2 -o encode/PythagoreanTriples
 gcc tools/drat-trim.c    -O2 -o tools/drat-trim
 gcc tools/color-encode.c -O2 -o tools/color-encode
 cd march_cu
--- encode/PythagoreanTriples.c
+++ encode/PythagoreanTriples.c
@@ -3,25 +3,25 @@
 #include <math.h>
 
 int main (int argc, char** argv) {
-  int a, b, c, max, offset = 0, nVars = 0, nClauses = 0;
+  int nVars = 0, nClauses = 0;
 
   if (argc < 2) {
     printf ("run using ./PythagoreanTriples MAX\n"); exit (0); }
 
-  max = atoi (argv[1]);
+  int max = atoi (argv[1]);
 
-  for (a = 1; a <= max; a++)
-    for (b = a; (a*a + b*b) <= (max * max); b++) {
-      c = sqrt(a*a + b*b);
+  for (int a = 1; a <= max; a++)
+    for (int b = a; (a*a + b*b) <= (max * max); b++) {
+      int c = sqrt(a*a + b*b);
       if ((c*c) == (a*a + b*b)) {
         nClauses += 2;
         if (c > nVars) nVars = c; } }
 
   printf ("p cnf %i %i\n", nVars, nClauses);
 
-  for (a = 1; a <= max; a++)
-    for (b = a; (a*a + b*b) <= (max*max); b++) {
-      c = sqrt(a*a + b*b);
+  for (int a = 1; a <= max; a++)
+    for (int b = a; (a*a + b*b) <= (max*max); b++) {
+      int c = sqrt(a*a + b*b);
       if ((c*c) == (a*a + b*b)) {
         printf (" %i  %i  %i 0\n", a, b, c);
         printf ("-%i -%i -%i 0\n", a, b, c); } }
